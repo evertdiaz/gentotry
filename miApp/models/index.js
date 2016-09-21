0f68b5e9ccb4f1abce7fb4cf7fdd8d8edc71b99d
Evert Diaz <evertdiazb@gmail.com>
2016-09-21 01:12:09 -0500

    Añadidos Schemas en modelos

--- miApp/models/index.js
+++ miApp/models/index.js
@@ -1,8 +1,19 @@
 var mongoose = require('mongoose')
-mongoose.model('users', {name: String})
+
+var Schema =  mongoose.Schema
+
+var userSchema = new Schema({
+	name: String,
+	age: Number
+})
+mongoose.model('users', userSchema) 
 var User = mongoose.model('users')
 
-mongoose.model('clothes', {tipo: String})
+var clothesSchema = new Schema({
+	tipo: String,
+	precio: Number
+})
+mongoose.model('clothes', clothesSchema)
 var clothes = mongoose.model('clothes')
 
 module.exports.User = User
