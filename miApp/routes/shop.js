d3e1ec29c46e208d58df5bb7690288093fed4ee7
Evert Diaz <evertdiazb@gmail.com>
2016-09-25 22:22:39 -0500

    Agregados enlaces en index

--- miApp/routes/shop.js
+++ miApp/routes/shop.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-	res.render('shop', {title: "Gento"})
+	res.render('shop', {title: "Comprar - Gento"})
 	// Para leer el query entramos en localhost:3000/?=age=22 por ejemplo
 	console.log(req.query)
 })
